8f1e9ecf7f9ab581618109850d39cc649ec3d685
israeloyeyemi28 <israeloyeyem28@gmail.com>
2024-07-16 00:17:42 +0100

    site-plan errors

--- carebuddies/script/main.js
+++ carebuddies/script/main.js
@@ -5,3 +5,17 @@ hamBurgerElement.addEventListener("click", () => {
   navElement.classList.toggle("open");
   hamBurgerElement.classList.toggle("open");
 });
+// scroll effect
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     } else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
+
+// const hiddenElements = document.querySelector(".div");
+// hiddenElements.forEach((el) => observer.observe(el));
