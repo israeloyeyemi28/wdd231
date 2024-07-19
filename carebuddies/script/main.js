8f1b9240aec48cfb5b8e2e7bf9b44ec7c18e1d33
israeloyeyemi28 <israeloyeyem28@gmail.com>
2024-07-19 14:57:44 +0100

    mistakes corrected

--- carebuddies/script/main.js
+++ carebuddies/script/main.js
@@ -5,20 +5,6 @@ hamBurgerElement.addEventListener("click", () => {
   navElement.classList.toggle("open");
   hamBurgerElement.classList.toggle("open");
 });
-// scroll effect
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//     } else {
-//       entry.target.classList.remove("show");
-//     }
-//   });
-// });
-// const hiddenElements = document.querySelector(".div");
-// hiddenElements.forEach((el) => observer.observe(el));
-
 const cards = document.querySelector(".grid");
 import updatePageVisitsCount from "../script/pageCount.js";
 import { fetchSitters } from "../script/api.js";
