8979a9a902a60702e5ff7d373b0bc9d1d0b2af4c
israeloyeyemi28 <israeloyeyem28@gmail.com>
2024-07-19 15:16:04 +0100

    efsd

--- carebuddies/script/dom.js
+++ carebuddies/script/dom.js
@@ -3,7 +3,6 @@ const cards = document.querySelector(".grid");
 export function displaySitters(data) {
   cards.innerHTML = ``;
   data.carebuddies.forEach((data) => {
-    // let select = document.getElementById("sitter-name");
     console.table(data["dominant-color"], data["color"], data.skills);
     let card = document.createElement("section");
     let name = document.createElement("h2");
@@ -12,12 +11,10 @@ export function displaySitters(data) {
     let type = document.createElement("h4");
     let phone = document.createElement("h4");
     let pic = document.createElement("img");
-    let details = document.createElement("button");
-    // let option = document.createElement("option");
+    let details = document.createElement("a");
     details.className = "more";
     name.innerHTML = `<bold>${data.name}</bold>`;
     type.innerHTML = `Type: ${data.Type}`;
-    // console.table(data.skills);
     phone.innerHTML = `${data.phone}`;
     pic.setAttribute("src", data.image);
     pic.setAttribute("alt", `pic of ${data.name}`);
@@ -32,16 +29,10 @@ export function displaySitters(data) {
       card.style.color = "#fff";
     }
     details.innerHTML = `Pick Sitter`;
+    details.setAttribute("href", "bookings.html")
     details.style.width = "50%";
     details.style.padding = "10px";
     details.style.margin = "0 auto";
-    //display the selct options
-    // option.textContent = data.name;
-    // option.value = data.name;
-    // console.log(option.value);
-    // select.append(option)
-    // select.appendChild(option);
-    // console.log(select);
     div.appendChild(pic);
     div.appendChild(name);
     div.appendChild(type);
@@ -53,7 +44,6 @@ export function displaySitters(data) {
     card.appendChild(phone);
     card.appendChild(details);
     card.appendChild(div);
-    // card.appendChild(option);
     card.addEventListener("click", () => openModal(data));
     cards.appendChild(card);
   });
